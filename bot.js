5c6d9342eda3cad4d7976f9b97e2d0729c5f7484
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2020-05-03 06:13:40 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,104 @@
+const Discord = require('discord.js');
+const fs = require("fs"); 
+const client = new Discord.Client();
+//////////////////////////////////////////////////////////////////////////////////////////
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('The Prince : MdAx77x')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('Created by: MdAx7zx .')
+  console.log('')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Sl-Rp Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Welcome to evil gates.`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} servers.`);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+ 
+});
+
+client.on('ready', function(){    
+  var ms = 5000 ;    
+  var setGame = ["SL-RP RoleyPlay",`Servers : ${client.guilds.size} | Users : ${client.users.size}`];    
+  var i = -1;    
+  var j = 0;    
+  setInterval(function (){    
+      if( i == -1 ){    
+j = 1;    
+     }    
+      if( i == (setGame.length)-1 ){    
+          j = -1;    
+    }    
+     i = i+j;    
+      client.user.setGame(setGame[i],`http://twitch.tv/MdAx7zx`);    
+}, ms);    
+  
+});
+///////////////////////////////////////////////////////////////////////////////////////////
+
+ client.on('message', message => {
+	    var prefix = "$";
+              if(!message.channel.guild) return;
+    if(message.content.startsWith(prefix + 'bc')) {
+    if(!message.channel.guild) return message.channel.send('**This Command Only For Servers**').then(m => m.delete(5000));
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**You Dont Have perms** `ADMINISTRATOR`' );
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+    let copy = "SL-RP BOT";
+    let request = `Requested By ${message.author.username}`;
+    if (!args) return message.reply('**Write Some Things To Broadcast**');message.channel.send(`**Are You Sure \nThe Broadcast: ** \` ${args}\``).then(msg => {
+    msg.react('✅')
+    .then(() => msg.react('❌'))
+    .then(() =>msg.react('✅'))
+    
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+    
+    let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+ reaction1.on("collect", r => {
+    message.channel.send(`**☑ | Done ... The Broadcast Message Has Been Sent To __${message.guild.members.size}__ Members**`).then(m => m.delete(5000));
+    message.guild.members.forEach(m => {
+  
+  var bc = new
+       Discord.RichEmbed()
+       .setColor('RANDOM')
+       .setTitle('Broadcast')
+       .addField('Server', message.guild.name)
+       .addField('Sender', message.author.username)
+       .addField('Message', args)
+.setFooter('Made By <@335484868479811584>')
+       .setThumbnail(message.author.avatarURL)
+       .setFooter(copy, client.user.avatarURL);
+    m.send({ embed: bc })
+    msg.delete();
+    })
+    })
+    reaction2.on("collect", r => {
+    message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
+    msg.delete();
+    })
+    })
+    }
+    });
+
+
+
+client.login(process.env.BOT_TOKEN);
